d209d696b531e535f971e6194e301a80a00225a9
yongshuang <yongshuang@gdgyun.com>
2019-06-11 10:30:24 +0800

    add webpack config and install webpack webpack-cli webpack-server-dev

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+module.exports ={
+    mode:"development",
+    entry:["./src/index.js"],
+    output:{
+        path:path.join(__dirname,dist),
+        filename:"bundle.js"
+    },
+    module:{},
+    plugins:[],
+    devServer:{}
+}
